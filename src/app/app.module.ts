0609748cdda40aa002fcc8295f10be55b2444f11
Marlon <marlon.haensdieke@check24.de>
2019-02-27 19:27:00 +0100

    Add pwa setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MembersModule } from './members/members.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, MembersModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        MembersModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: environment.production,
+        }),
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
