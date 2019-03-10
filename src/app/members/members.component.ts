b63b5e5475037f4d5084e990e7aa5a41751d7fef
Marlon <marlon.haensdieke@check24.de>
2019-03-10 19:56:09 +0100

    Fetch and show members collection

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import {
+    AngularFirestore,
+    AngularFirestoreCollection,
+    CollectionReference,
+} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+export interface Member {
+    firstname: string;
+    lastname: string;
+    userUid: string;
+}
 
 @Component({
     selector: 'app-members',
@@ -6,7 +18,17 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./members.component.scss'],
 })
 export class MembersComponent implements OnInit {
-    constructor() {}
+    private membersCollection: AngularFirestoreCollection<Member>;
+    public members: Observable<Member[]>;
+
+    constructor(private angularFireStore: AngularFirestore) {
+        this.membersCollection = this.angularFireStore.collection(
+            'members'
+            // (ref: CollectionReference) => ref.where('firstname', '==', 'Marlon')
+        );
+
+        this.members = this.membersCollection.valueChanges();
+    }
 
     public ngOnInit(): void {}
 }
--- src/app/members/members.module.ts
+++ src/app/members/members.module.ts
@@ -1,11 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { MaterialModule } from '../material/material.module';
 import { MembersRoutingModule } from './members-routing.module';
 import { MembersComponent } from './members.component';
 
 @NgModule({
     declarations: [MembersComponent],
-    imports: [CommonModule, MaterialModule, MembersRoutingModule],
+    imports: [
+        CommonModule,
+        MaterialModule,
+        MembersRoutingModule,
+        AngularFirestoreModule,
+    ],
 })
 export class MembersModule {}
