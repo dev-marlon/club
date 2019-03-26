3c92b6fafe1063f34b6749559c3961794110c73e
Marlon <marlon.haensdieke@check24.de>
2019-03-26 15:38:28 +0100

    Sort members alphabetical by lastname

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { MembersService } from './members.service';
 import { Member } from './models/member.interface';
 
@@ -12,6 +13,12 @@ export class MembersComponent {
     public members$: Observable<Member[]>;
 
     constructor(private membersService: MembersService) {
-        this.members$ = this.membersService.members$;
+        this.members$ = this.membersService.members$.pipe(
+            map((members: Member[]) =>
+                members.sort((a: Member, b: Member) =>
+                    a.lastname < b.lastname ? -1 : 1
+                )
+            )
+        );
     }
 }
