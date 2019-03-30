218c05fe32f90d155913ca558b350f55fbcd32a8
Marlon <marlon.haensdieke@check24.de>
2019-03-30 17:06:10 +0100

    Filter by name and categories

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { combineLatest, Observable, of } from 'rxjs';
+import { debounce, delay, map, startWith } from 'rxjs/operators';
 import { MembersService } from './members.service';
 import { Member } from './models/member.interface';
 
@@ -11,6 +12,21 @@ import { Member } from './models/member.interface';
 })
 export class MembersComponent {
     public members$: Observable<Member[]>;
+    public filteredMembers$: Observable<Member[]>;
+
+    public form: FormGroup = new FormGroup({
+        searchString: new FormControl('', [Validators.minLength(2)]),
+        categories: new FormControl(''),
+    });
+    public categories: string[] = [
+        'Beirat',
+        'Ehrenmitglied',
+        'Trainer',
+        'Vorstand',
+    ];
+
+    private searchInput$: Observable<string>;
+    public categoriesSelect$: Observable<string[]>;
 
     constructor(private membersService: MembersService) {
         this.members$ = this.membersService.members$.pipe(
@@ -20,5 +36,84 @@ export class MembersComponent {
                 )
             )
         );
+
+        const searchStringMinLength = 2;
+        const debounceSearch = 0;
+
+        this.searchInput$ = this.form.get('searchString').valueChanges.pipe(
+            startWith(''),
+            map((searchString: string) =>
+                searchString.length >= searchStringMinLength ? searchString : ''
+            ),
+            debounce((val: any) => {
+                return of(true).pipe(
+                    delay(
+                        val.length >= searchStringMinLength ? debounceSearch : 0
+                    )
+                );
+            })
+        );
+
+        this.categoriesSelect$ = this.form
+            .get('categories')
+            .valueChanges.pipe(startWith([]));
+
+        this.filteredMembers$ = combineLatest(
+            this.members$,
+            this.searchInput$,
+            this.categoriesSelect$
+        ).pipe(
+            map(
+                ([members, searchString, selectedCategories]: [
+                    Member[],
+                    string,
+                    string[]
+                ]) => {
+                    let filteredMembers = members;
+
+                    if (searchString.length >= searchStringMinLength) {
+                        const _searchString = searchString.toLowerCase();
+
+                        filteredMembers = filteredMembers.filter(
+                            (member: Member) => {
+                                return (
+                                    member.firstname
+                                        .toLowerCase()
+                                        .indexOf(_searchString) !== -1 ||
+                                    member.lastname
+                                        .toLowerCase()
+                                        .indexOf(_searchString) !== -1
+                                );
+                            }
+                        );
+                    }
+
+                    if (selectedCategories.length > 0) {
+                        filteredMembers = filteredMembers.filter(
+                            (member: Member) => {
+                                return (
+                                    selectedCategories.indexOf(
+                                        member.category
+                                    ) !== -1
+                                );
+                            }
+                        );
+                    }
+
+                    return filteredMembers;
+                }
+            )
+        );
+    }
+
+    public removeFilterChip(category: string): void {
+        const selectedCategories: string[] = this.form.get('categories').value;
+        this.form
+            .get('categories')
+            .setValue(
+                selectedCategories.filter(
+                    (_category: string) => _category !== category
+                )
+            );
     }
 }
--- src/app/members/models/member.interface.ts
+++ src/app/members/models/member.interface.ts
@@ -2,5 +2,6 @@ export interface Member {
     uid: string;
     firstname: string;
     lastname: string;
+    category: string;
     userUid: string;
 }
