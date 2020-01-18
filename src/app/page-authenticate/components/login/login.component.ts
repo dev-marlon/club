6245a7983968f015beec5da4c1dbb56628e2d5d7
Marlon Hänsdieke <marlon.haensdieke@check24.de>
2020-01-18 20:30:04 +0100

    Break down login component

--- src/app/page-authenticate/components/login/login.component.ts
+++ src/app/page-authenticate/components/login/login.component.ts
@@ -1,13 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { MatSnackBar } from '@angular/material';
-import { ActivatedRoute, Router } from '@angular/router';
-import { User } from 'firebase';
-import { Observable } from 'rxjs';
-import { AuthService } from '../../services/auth.service';
 
-export interface LoginConfig {
-    defaultRedirectToOnSuccess: string;
+export interface LoginData {
+    email: string;
+    password: string;
 }
 
 @Component({
@@ -16,56 +12,18 @@ export interface LoginConfig {
     styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-    public disableForm: boolean = false;
-    public user: Observable<User | null>;
-    private config: LoginConfig = null;
+    @Input() public formDisabled: boolean;
+    @Output() public login: EventEmitter<LoginData> = new EventEmitter();
 
     public loginForm: FormGroup = new FormGroup({
         email: new FormControl('', [Validators.required, Validators.email]),
         password: new FormControl('', Validators.required),
     });
 
-    constructor(
-        private authService: AuthService,
-        private router: Router,
-        private snackBar: MatSnackBar,
-        private activatedRoute: ActivatedRoute
-    ) {
-        this.activatedRoute.data.subscribe((config: LoginConfig) => {
-            this.config = config;
-        });
-
-        this.user = this.authService.user;
-    }
-
     public onSubmit(): void {
-        this.authService
-            .login(this.loginForm.value.email, this.loginForm.value.password)
-            .then(() => {
-                if (this.authService.redirectUrl) {
-                    this.redirectTo(this.authService.redirectUrl);
-                }
-
-                if (this.config.defaultRedirectToOnSuccess) {
-                    this.redirectTo(this.config.defaultRedirectToOnSuccess);
-                }
-            })
-            .catch((error: any) => {
-                const snackBarRef = this.snackBar.open(error.code, null, {
-                    duration: 3000,
-                    horizontalPosition: 'center',
-                    verticalPosition: 'top',
-                });
-
-                this.disableForm = true;
-
-                snackBarRef.afterDismissed().subscribe(() => {
-                    this.disableForm = false;
-                });
-            });
-    }
-
-    private redirectTo(target: string): void {
-        this.router.navigate([target]);
+        this.login.emit({
+            email: this.loginForm.value.email,
+            password: this.loginForm.value.password,
+        });
     }
 }
--- src/app/page-authenticate/page-authenticate.component.ts
+++ src/app/page-authenticate/page-authenticate.component.ts
@@ -1,8 +1,66 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from 'firebase';
+import { Observable } from 'rxjs';
+import { LoginData } from './components/login/login.component';
+import { AuthService } from './services/auth.service';
+
+export interface PageAuthenticateConfig {
+    defaultRedirectToOnSuccess: string;
+}
 
 @Component({
     selector: 'page-authenticate',
     templateUrl: './page-authenticate.component.html',
     styleUrls: ['./page-authenticate.component.scss'],
 })
-export class PageAuthenticateComponent {}
+export class PageAuthenticateComponent {
+    public loginFormDisabled: boolean = false;
+    public user: Observable<User | null>;
+
+    private config: PageAuthenticateConfig = null;
+
+    constructor(
+        private authService: AuthService,
+        private activatedRoute: ActivatedRoute,
+        private router: Router,
+        private snackBar: MatSnackBar
+    ) {
+        this.activatedRoute.data.subscribe((config: PageAuthenticateConfig) => {
+            this.config = config;
+        });
+        this.user = this.authService.user;
+    }
+
+    public onLogin(loginData: LoginData): void {
+        this.authService
+            .login(loginData.email, loginData.password)
+            .then(() => {
+                if (this.authService.redirectUrl) {
+                    this.redirectTo(this.authService.redirectUrl);
+                }
+
+                if (this.config.defaultRedirectToOnSuccess) {
+                    this.redirectTo(this.config.defaultRedirectToOnSuccess);
+                }
+            })
+            .catch((error: any) => {
+                const snackBarRef = this.snackBar.open(error.code, null, {
+                    duration: 3000,
+                    horizontalPosition: 'center',
+                    verticalPosition: 'top',
+                });
+
+                this.loginFormDisabled = true;
+
+                snackBarRef.afterDismissed().subscribe(() => {
+                    this.loginFormDisabled = false;
+                });
+            });
+    }
+
+    private redirectTo(target: string): void {
+        this.router.navigate([target]);
+    }
+}
